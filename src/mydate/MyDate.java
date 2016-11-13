8774373e222453b9ab91b143d38ec64436ea4764
Dashia <das-bobkova@yandex.ru>
2016-11-14 00:00:21 +0300

    Methods: equals() & hashCode() override for MyDate class - done / all class not finished

--- src/mydate/MyDate.java
+++ src/mydate/MyDate.java
@@ -1,9 +1,86 @@
 package mydate;
 
+
 /**
  * Created by Daria Serebryakova on 13.11.2016.
  */
 public class MyDate {
+    public MyDate(int year, int month, int day) {
+        this.year = year;
+        this.month = month-1;
+        this.day = day;
+    }
+    public void setDate(int year, int month, int day){
+        this.year = year;
+        this.month = month -1;
+        this.day = day;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public void setMonth(int month) {
+        this.month = month;
+    }
+
+    public void setDay(int day) {
+        this.day = day;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public int getMonth() {
+        return month;
+    }
+
+    public int getDay() {
+        return day;
+    }
+
+    public int getDayOfWeek(int year, int month, int day){
+
+
+
+        return 0;
+    }
+
+
+
+    @Override
+    public String toString() {
+        return "MyDate{" +
+                //strDays[getDayOfWeek(year, month, day)]+
+                " " + day +
+                " " + srtMonths[month] +
+                " " + year +
+
+
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {return true;}
+        if (!(obj instanceof MyDate)) {return false;}
+        MyDate date = (MyDate) obj;
+        return (this.year == date.year) && (this.month == date.month)&&(this.day == date.day);
+
+    }
+
+    @Override
+    public int hashCode() {
+        int result = 17;
+        result = 31 * result + year;
+        result = 31 * result + month;
+        result = 31 * result + day;
+
+        return result;
+    }
+
+    //
     private int year;
     private int month;
     private int day;
--- src/mydate/Runner.java
+++ src/mydate/Runner.java
@@ -6,9 +6,16 @@ package mydate;
 public class Runner {
     public static void main(String[] args) {
 
+        MyDate date1 = new MyDate(2016, 12, 13);
+        System.out.println("date1 = " + date1);
+        MyDate date2 = new MyDate(2016, 11, 13);
+        System.out.println("date2 = " + date2);
 
+        System.out.println(date1 == date2);
+        System.out.println(date1.equals(date2));
 
-
+        System.out.println(date1.hashCode());
+        System.out.println(date2.hashCode());
 
 
     }
