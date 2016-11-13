eeab71072a9c5c85ef6b7b55247c7e6f96cb1bb8
Dashia <das-bobkova@yandex.ru>
2016-11-13 23:05:35 +0300

    Methods: equals() & hashCode() override for MyDate class - begining

---
+++ src/mydate/MyDate.java
@@ -0,0 +1,20 @@
+package mydate;
+
+/**
+ * Created by Daria Serebryakova on 13.11.2016.
+ */
+public class MyDate {
+    private int year;
+    private int month;
+    private int day;
+    String[] srtMonths = {"Jan","Feb","Mar",
+                          "Apr","May","Jun",
+                          "Jul","Aug","Sep",
+                          "Oct","Nov","Dec"};
+    String[] strDays = {"Sunday","Monday","Tueasday",
+                        "Wednesday","Thursday","Friday","Saturday"};
+    int[] daysInMonth = {31,28,31,30,31,30,31,31,30,31,30,31};
+
+
+
+}
---
+++ src/mydate/Runner.java
@@ -0,0 +1,15 @@
+package mydate;
+
+/**
+ * Created by Daria Serebryakova on 13.11.2016.
+ */
+public class Runner {
+    public static void main(String[] args) {
+
+
+
+
+
+
+    }
+}
