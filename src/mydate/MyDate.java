904b95e9950f4d68559156b84d7e344db6bfc92b
dbobkova <das-bobkova@yandex.ru>
2016-11-14 13:52:50 +0400

    MyDate class - done.

--- src/mydate/MyDate.java
+++ src/mydate/MyDate.java
@@ -40,11 +40,153 @@ public class MyDate {
         return day;
     }
 
-    public int getDayOfWeek(int year, int month, int day){
+    public static boolean isLeapYear(int year){
+        boolean isLeap = false;
+        int isL = (year%100)%4;
 
+        if (isL == 0){isLeap = true;}
+        return isLeap;
+    }
+    public static boolean isValidDate(int year, int month, int day){
+        boolean isValid = true;
 
+        if(month < 0 || month >= 12 || month/100 != 0) {
+            isValid = false;
+            System.out.println("date is not valid");
+        }
 
-        return 0;
+        int maxDate = daysInMonth[month];
+         if (month == 1 && isLeapYear(year) == true) {
+            maxDate = 29;
+        }
+
+        if(day < 1 || day > maxDate|| month/100 != 0){
+            isValid = false;
+            System.out.println("date is not valid, max date in this month is " + maxDate );}
+
+        return isValid;
+    }
+
+    public static int getDayOfWeek(int year, int month, int day){
+        int dayOfWeek = -1;
+        int yearIndex = (year%100)/12 + (year%100)%12 + ((year%100)%12)/4;
+
+        int[] monthIndexes = {6,2,2,5,0,3,5,1,4,6,2,4};
+        int monthIndex = monthIndexes[month];
+
+        int centuaryIndex = 0;
+        if(year/1000 == 1){ centuaryIndex = 1;}
+
+        dayOfWeek = (yearIndex + monthIndex + day + centuaryIndex)%7 ;//0-Sunday, 1-Monday
+
+        return dayOfWeek;
+    }
+
+    public MyDate nextDay(){
+        if(isValidDate(year,month,day) == false) {
+            System.out.println("date is not valid");
+            return null;
+        }
+        int nDay = day;
+        int nMonth = month;
+        int nYear = year;
+
+        if(day <  daysInMonth[month]){
+            nDay = day + 1;
+            System.out.println("nDay = " + nDay);
+        }
+        else if(day ==  daysInMonth[month] && month ==11){ //11-DEC
+            nDay = 1;
+            nMonth = 0;
+            nYear = year + 1;
+        }
+        else if(day ==  daysInMonth[month] ){
+            nDay = 1;
+            nMonth = month + 1;
+        }
+        MyDate date = new MyDate(nYear, nMonth+1, nDay);
+        return date;
+    }
+
+    public MyDate previousDay(){
+        if(isValidDate(year,month,day) == false) {
+            System.out.println("date is not valid");
+            return null;
+        }
+        int nDay = day;
+        int nMonth = month;
+        int nYear = year;
+
+        if(day > 1){
+            nDay = day - 1;
+            System.out.println("nDay = " + nDay);
+        }
+        else if(day ==  1 && month == 0){ //11-DEC 0 -Jan
+            nMonth = 11;
+            nDay = daysInMonth[month];
+
+            nYear = year - 1;
+        }
+        else if(day ==  1 ){
+            nMonth = month - 1;
+            nDay = daysInMonth[month];
+
+        }
+        MyDate date = new MyDate(nYear, nMonth+1, nDay);
+        return date;
+    }
+    public MyDate nextMonth(){
+        if(isValidDate(year,month,day) == false) {
+            return null;
+        }
+        int nDay = day;
+        int nMonth = month;
+        int nYear = year;
+
+        if(month  < 11){ //11-DEC
+            nMonth = month + 1;
+        }
+        else if(month  == 11 ){
+            nMonth = 0;
+            nYear = year + 1;
+        }
+        MyDate date = new MyDate(nYear, nMonth+1, nDay);
+        return date;
+    }
+    public MyDate previousMonth(){
+        if(isValidDate(year,month,day) == false) {
+            return null;
+        }
+        int nDay = day;
+        int nMonth = month;
+        int nYear = year;
+
+        if(month  > 0){ //11-DEC
+            nMonth = month - 1;
+        }
+        else if(month  == 0 ){
+            nMonth = 11;
+            nYear = year - 1;
+        }
+        MyDate date = new MyDate(nYear, nMonth+1, nDay);
+        return date;
+    }
+
+    public MyDate nexYear(){
+        if(isValidDate(year,month,day) == false) {
+            return null;
+        }
+
+        MyDate date = new MyDate(year + 1, month + 1, day);
+        return date;
+    }
+    public MyDate previousYear(){
+        if(isValidDate(year,month,day) == false) {
+            return null;
+        }
+
+        MyDate date = new MyDate(year - 1, month + 1, day);
+        return date;
     }
 
 
@@ -52,12 +194,10 @@ public class MyDate {
     @Override
     public String toString() {
         return "MyDate{" +
-                //strDays[getDayOfWeek(year, month, day)]+
+                strDays[getDayOfWeek(year, month, day)]+
                 " " + day +
                 " " + srtMonths[month] +
                 " " + year +
-
-
                 '}';
     }
 
@@ -84,13 +224,13 @@ public class MyDate {
     private int year;
     private int month;
     private int day;
-    String[] srtMonths = {"Jan","Feb","Mar",
-                          "Apr","May","Jun",
-                          "Jul","Aug","Sep",
-                          "Oct","Nov","Dec"};
-    String[] strDays = {"Sunday","Monday","Tueasday",
+    private  static String[] srtMonths = {"Jan","Feb","Mar", //0,1,2
+                          "Apr","May","Jun",//3,4,5
+                          "Jul","Aug","Sep",//6,7,8
+                          "Oct","Nov","Dec"};//9,10,11
+    private static String[] strDays = {"Sunday","Monday","Tueasday",
                         "Wednesday","Thursday","Friday","Saturday"};
-    int[] daysInMonth = {31,28,31,30,31,30,31,31,30,31,30,31};
+    private static int[] daysInMonth = {31,28,31,30,31,30,31,31,30,31,30,31};
 
 
 
--- src/mydate/Runner.java
+++ src/mydate/Runner.java
@@ -6,16 +6,32 @@ package mydate;
 public class Runner {
     public static void main(String[] args) {
 
-        MyDate date1 = new MyDate(2016, 12, 13);
+        MyDate date1 = new MyDate(2016, 11, 14);
         System.out.println("date1 = " + date1);
-        MyDate date2 = new MyDate(2016, 11, 13);
+
+        MyDate date2 = new MyDate(2016, 11, 14);
         System.out.println("date2 = " + date2);
 
         System.out.println(date1 == date2);
         System.out.println(date1.equals(date2));
 
         System.out.println(date1.hashCode());
-        System.out.println(date2.hashCode());
+        System.out.println(date2.hashCode() +"\n\n");
+
+
+
+        boolean isValid = MyDate.isValidDate(2016, 2, 29);
+
+        MyDate date3 = new MyDate(2016, 11, 1);
+        System.out.println("date3 = " + date3);
+        System.out.println("nextDay = " + date3.nextDay());
+        System.out.println("previousDay = " + date3.previousDay());
+        System.out.println("nextMonth = " + date3.nextMonth());
+        System.out.println("previousMonth = " + date3.previousMonth());
+        System.out.println("nextYear = " + date3.nexYear());
+        System.out.println("previousYear = " + date3.previousYear());
+
+        System.out.println();
 
 
     }
